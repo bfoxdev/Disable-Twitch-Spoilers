91f32a9599d5642a6bdc2fdcfd1613e1fec30aa8
ben <ben@bfox.dev>
2022-12-08 15:29:54 -0500

    popup redesign

--- popup/menu.js
+++ popup/menu.js
@@ -36,6 +36,21 @@ function listenerAll() {
   })
 }
 
+function switchIcon() {
+  console.log('switching icon')
+  browser.browserAction.setIcon({
+    path: {
+      32: 'ez-noglasses-32.webp',
+    },
+  })
+}
+
+//the browser.setIcon should check if there is a style tag with an id of vodremover or percentageremover and if there is, change the icon to the active icon
+//if there is not, change the icon to the inactive icon
+// browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+//   browser.tabs.sendMessage(tabs[0].id, { command: 'check' })
+// })
+
 document
   .getElementById('timestamp')
   .addEventListener('click', listenerTimestamp)
@@ -48,3 +63,4 @@ document
   .getElementById('timestampjs')
   .addEventListener('click', listenerTimestampJS)
 document.getElementById('all').addEventListener('click', listenerAll)
+document.getElementById('icon').addEventListener('click', switchIcon)
